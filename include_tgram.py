70e9b87bcb6824b8758cf5f5108e968327eda9f7
Gioxx <giovanni@solone.it>
2022-08-31 12:44:50 +0200

    Clean.

--- include_tgram.py
+++ include_tgram.py
@@ -7,8 +7,6 @@ import os
 import os.path
 import sys
 
-# env_file = os.getenv('GITHUB_ENV') # https://stackoverflow.com/a/40698307
-
 def sendtotelegram(message):
     #load_dotenv(find_dotenv())
     token = os.environ.get("TGRAMTOKEN").strip()
