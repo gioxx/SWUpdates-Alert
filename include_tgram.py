900389e3500cfaed3bc0806339b16ee074145330
Gioxx <giovanni@solone.it>
2022-08-31 11:57:52 +0200

    Fix Telegram def.

--- include_tgram.py
+++ include_tgram.py
@@ -1,4 +1,4 @@
-from dotenv import load_dotenv, find_dotenv
+#from dotenv import load_dotenv, find_dotenv
 import requests
 import urllib.request
 import base64
@@ -7,10 +7,10 @@ import os
 import os.path
 import sys
 
-env_file = os.getenv('GITHUB_ENV') # https://stackoverflow.com/a/40698307
+# env_file = os.getenv('GITHUB_ENV') # https://stackoverflow.com/a/40698307
 
 def sendtotelegram(message):
-    load_dotenv(find_dotenv())
+    #load_dotenv(find_dotenv())
     token = os.environ.get("TGRAMTOKEN").strip()
     chatid = os.environ.get("TGRAMCHATID").strip()
     url = "https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s" % (token, chatid, message)
