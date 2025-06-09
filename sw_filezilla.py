f8ef6504dea53f7f790b33f5da29154c0437ce3c
Giovanni Francesco Solone <giovanni@solone.it>
2025-06-09 17:35:23 +0200

    Use update API for FileZilla

--- sw_filezilla.py
+++ sw_filezilla.py
@@ -1,32 +1,18 @@
 import os
-import re
 import requests
-from core.version_check import run_check
+from core.version_check import fetch_json, run_check
 
 
-URL = "https://filezilla-project.org/download.php?show_all=1"
-
-
-def get_latest_version_from_html(url):
-    session = requests.Session()
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-    }
-    try:
-        response = session.get(url, headers=headers, timeout=10)
-        response.raise_for_status()
-        html_content = response.text
-        match = re.search(r'FileZilla_(\d+\.\d+\.\d+)_', html_content)
-        if match:
-            return match.group(1)
-    except requests.RequestException as e:
-        print(f"Error fetching the page: {e}")
-    return None
+URL = "https://update.filezilla-project.org/update.php?type=client&channel=release&format=1"
 
 
 def fetch_filezilla():
-    version = get_latest_version_from_html(URL)
-    return {"version": version}
+    try:
+        data = fetch_json(URL)
+        return data.get("version")
+    except requests.RequestException as e:
+        print(f"Error fetching FileZilla update information: {e}")
+        return None
 
 
 if __name__ == "__main__":
